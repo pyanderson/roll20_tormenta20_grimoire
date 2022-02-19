1910be561b36079b007968b36aa2a1dda64da140
Anderson Lima <anderson.sl93@hotmail.com>
2022-02-19 01:37:39 -0300

    add tormenta20 book files

---
+++ src/tormenta20_book.js
@@ -0,0 +1,147 @@
+'use strict'
+
+
+function render_modal_info(name, description) {
+  const dummy_slug = name.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '-');
+  const modal = `
+    <div id="tormenta20-info-${dummy_slug}" class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-draggable ui-resizable" style="outline: currentcolor none 0px; z-index: 10517; position: fixed; height: 390px; width: 552px; top: 121px; left: 540px; display: block;" tabindex="-1" role="dialog" aria-labelledby="ui-id-${dummy_slug}">
+      <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix">
+        <span id="ui-id-${dummy_slug}" class="ui-dialog-title">${name}</span>
+        <a href="#" class="ui-dialog-titlebar-close ui-corner-all" role="button" id="tormenta20-close-buuton-${dummy_slug}"><span class="ui-icon ui-icon-closethick">close</span></a>
+      </div>
+      <div class="ui-resizable-handle ui-resizable-n" style="z-index: 1000;"></div>
+      <div class="ui-resizable-handle ui-resizable-e" style="z-index: 1000;"></div>
+      <div class="ui-resizable-handle ui-resizable-s" style="z-index: 1000;"></div>
+      <div class="ui-resizable-handle ui-resizable-w" style="z-index: 1000;"></div>
+      <div class="ui-resizable-handle ui-resizable-se ui-icon ui-icon-gripsmall-diagonal-se ui-icon-grip-diagonal-se" style="z-index: 1000;"></div>
+      <div class="ui-resizable-handle ui-resizable-sw" style="z-index: 1000;"></div>
+      <div class="ui-resizable-handle ui-resizable-ne" style="z-index: 1000;"></div>
+      <div class="ui-resizable-handle ui-resizable-nw" style="z-index: 1000;"></div>
+      <div id="tormenta20-info-content-${dummy_slug}" class="dialog ui-dialog-content ui-widget-content" style="display: block; width: auto; min-height: 0px; height: 303px;" scrolltop="0" scrollleft="0">
+        <div class="dialog largedialog handoutviewer" style="display: block;">
+           <div style="padding: 10px;">
+              <div class="row-fluid">
+                 <div class="span12">
+                    <div class="content note-editor notes">${description}</div>
+                    <div class="clear"></div>
+                 </div>
+              </div>
+           </div>
+        </div>
+      </div>
+    </div>
+    `;
+  $('body').append(modal);
+  $(`#tormenta20-info-${dummy_slug}`).draggable({handle: 'div.ui-dialog-titlebar'});
+  $(`#tormenta20-close-buuton-${dummy_slug}`).on('click', function() {
+    $(`#tormenta20-info-${dummy_slug}`).remove();
+  });
+}
+
+function render_info_template(name, description) {
+  return `&{template:t20-info}{{infoname=${name}}}{{description=${description}}}`;
+}
+
+function active_tree_view() {
+  const toggler = document.getElementsByClassName('tormenta20-book-folder');
+  for (let i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener('click', function() {
+      this.parentElement.querySelector('.tormenta20-book-nested-folder').classList.toggle('tormenta20-book-active-folder');
+      this.classList.toggle('tormenta20-book-folder-open');
+    });
+  }
+
+  $('[name="tormenta20-chat-info-button"]').on('click', function() {
+    const div = $(this);
+    $('#textchat-input .ui-autocomplete-input').val(render_info_template(div.attr('item-name'), div.attr('item-description')));
+    $('#textchat-input .btn').click();
+  });
+
+  $('[name="tormenta20-modal-info-button"]').on('click', function() {
+    const div = $(this);
+    render_modal_info(div.attr('item-name'), div.attr('item-description')); 
+  });
+}
+
+function render_book_item(item) {
+  return `
+  <li class="tormenta20-book-row journalitem dd-item">
+    <div class="tormenta20-book-chat-icon" name="tormenta20-chat-info-button" item-name="${item.name}" item-description="${item.description}">
+      <a class="pictos">q</a>
+    </div>
+    <div class="tormenta20-book-item-name dd-content" name="tormenta20-modal-info-button" item-name="${item.name}" item-description="${item.description}">
+      ${item.name}
+    </div>
+  </li>`;
+}
+
+function render_book_folder(folder) {
+  let content = `
+    <li class="dd-item dd-folder">
+      <span class="tormenta20-book-folder dd-content">${folder.name}</span>
+      <ul class="tormenta20-book-nested-folder">
+  `;
+  for (let i = 0; i < folder.items.length; i++)
+    content += render_book_element(folder.items[i]);
+  return content + `</ul></li>`;
+}
+
+function render_book_element(element) {
+  if (element.type == 'folder')
+    return render_book_folder(element);
+  return render_book_item(element);
+}
+
+function render_book_content(rules) {
+  let content = `<ul id="tormenta20_book_content" class="dd-list dd folderroot">`;
+  for (let i = 0; i < rules.length; i++)
+    content += render_book_element(rules[i]);
+  return content + `</ul>`;
+}
+
+function render_book_tab(rules, retry=20) { // wait the tab for 20 seconds then give up
+  if (retry <= 0) return;
+  const rightsidebar = $('#rightsidebar');
+  const settings_tab = rightsidebar.find('[aria-controls="vm_settings_categories"][role="tab"]')
+  const journal_content = rightsidebar.find('#journal');
+  rightsidebar.find('[aria-controls="tormenta20_book"][role="tab"]').remove();
+  rightsidebar.find("#tormenta20_book").remove();
+
+  if(settings_tab.length == 0) {
+    return setTimeout(() => {render_book_tab(retry-1)}, 1000); // wait one second and try again
+  }
+
+  const book_tab = settings_tab.clone(true);
+  const book_content = journal_content.clone(true).html(render_book_content(rules));
+
+  // set tab attrs
+  book_tab
+    .attr('title', 'Tormenta20 Rules')
+    .attr('aria-controls', 'tormenta20_book')
+    .attr('aria-labelledby', 'ui-id-tormenta20');
+  book_tab.find('a')
+    .attr('id', 'ui-id-tormenta20')
+    .attr('href', '#tormenta20_book')
+    .html('u');
+
+  // set content attrs
+  book_content
+    .attr('id', 'tormenta20_book')
+    .attr('aria-labelledby', 'ui-id-tormenta20');
+
+  // render the book
+  settings_tab.before(book_tab);
+  journal_content.after(book_content);
+
+  active_tree_view();
+}
+
+$(document).ready(function () {
+  window.addEventListener("message", function(event) {
+    if (event.source != window)
+      return;
+    if (event.data.type && (event.data.type == "FROM_CONTENT")) {
+      render_book_tab(JSON.parse(event.data.text));
+    }
+  }, false)
+})
