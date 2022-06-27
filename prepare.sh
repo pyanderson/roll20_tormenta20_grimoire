191d4d57c23857b9501f692774e5fac74e92787d
Doug Godinho <douglas@occupop.com>
2022-06-26 21:16:55 -0300

    prepare improved

--- prepare.sh
+++ prepare.sh
@@ -5,8 +5,8 @@ set -e
 if [[ "$1" =~ ^(chrome|firefox)$ ]]; then
     echo "preparing $1"
     cp "$1"/manifest.json src/manifest.json
-    [ -e "$1".zip ] && rm "$1".zip
-    cd src && zip -r ../"$1".zip * && cd ..
+    [ -e better-t20-"$1".zip ] && rm better-t20-"$1".zip
+    cd src && zip -r ../better-t20-"$1".zip * && cd ..
     echo "$1 done"
 else
     echo "invalid option: $1"
