75f5ba119a04a281fd2704d578aedefe76ed236e
Doug Godinho <douglas@occupop.com>
2022-07-04 21:26:17 -0300

    tracker button fixes and add counter option

--- src/modules/tracker.js
+++ src/modules/tracker.js
@@ -5,11 +5,19 @@ T20.modules.tracker = {
     const init = $('#initiativewindow')
     const settings = $('#initiativewindow_settings')
     await checkTimeout(() => init.is(':visible'))
-    init.dialog('option', 'minWidth', 220)
-    const foot = init.closest('.ui-dialog').find('.ui-dialog-buttonpane')
-    const clear = $(`<button class="btn btn-xs btn-danger">Clear`).click(() => settings.find('.clearlist').click())
-    const sort = $(`<button class="btn btn-xs">Sort`).click(() => settings.find('.sortlist_numericdesc').click())
-    foot.append(clear).append(sort)
+    init.dialog('option', 'minWidth', 270)
+    init.closest('.ui-dialog').find('.ui-dialog-buttonpane')
+      .append($(`<button class="btn btn-xs btn-danger clear">Clear`))
+      .append($(`<button class="btn btn-xs counter">Counter`))
+      .append($(`<button class="btn btn-xs sort">Sort`))
+
+    $body.on('click', '.btn.sort', () => settings.find('.sortlist_numericdesc').click())
+    $body.on('click', '.btn.clear', () => settings.find('.clearlist').click())
+    $body.on('click', '.btn.counter', () => {
+      settings.find('.customentry').val('** Rodada **')
+      settings.find('.customformula').val('1')
+      settings.find('.addcustom').click()
+    })
   },
   onSheet: ($iframe, characterId) => {}
 }
