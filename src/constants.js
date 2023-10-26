8713f98b394de799676c401629bde53826be403b
Anderson Lima <anderson.sl93@hotmail.com>
2023-10-26 02:19:02 -0300

    add constants

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+// Static Data
+const SPELLS_PATH = 'data/spells.json';
+const POWERS_PATH = 'data/powers.json';
+const BOOK_PATH = 'data/book.json';
+const CHARACTER_SHEET_CSS_PATH = 'css/sheet.css';
+const ICON_PATH = 'images/32.png';
+
+// Book
+const ZOOM_DIV_ID = 'zoomclick';
+const BOOK_BUTTON_ID = 'tormenta20-book-button';
+const BOOK_DIALOG_ID = 'tormenta20-dialog-book';
+const BOOK_LIST_ID = 'tormenta20-book-list';
