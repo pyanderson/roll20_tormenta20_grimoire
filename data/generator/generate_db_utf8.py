510f63b0c2a5a62d5b37a505ba317329f58eb110
raul-brilhante <raulninjin@gmail.com>
2025-01-18 10:58:43 -0300

    Adicionada as raças e magias do Ameaças de Arton.

---
+++ data/generator/generate_db_utf8.py
@@ -0,0 +1,103 @@
+import json
+from collections import OrderedDict
+from operator import itemgetter
+from pathlib import Path
+
+# Tive problemas em gerar o db então modifiquei para forçar a codificação em UTF-8.
+# Estou mandando isso junto apenas para informar, mesmo!
+
+output_path = "static/db.json"
+
+def load_folder(source_path):
+    folder = []
+    for path in Path(source_path).rglob("*.json"):
+        with open(path, encoding="utf-8") as jfile:
+            folder.append(json.load(jfile))
+    return folder
+
+def convert_to_book_folder(d):
+    items = d.get("abilities", d.get("powers", d.get("spells")))
+    return {
+        "type": "folder",
+        "name": d["name"],
+        "items": [
+            {
+                **item,
+                "spellType": item.get("type"),
+                "type": "item",
+            }
+            for item in items
+        ],
+    }
+
+# load data
+book = load_folder("data/book")
+races = load_folder("data/races")
+classes = load_folder("data/classes")
+powers = load_folder("data/powers")
+spells = load_folder("data/spells")
+equipments = load_folder("data/equipments")
+
+# fill the book
+for name, source in [
+    ("Raças", races),
+    ("Classes", classes),
+    ("Poderes", powers),
+    ("Magias", spells),
+]:
+    book.append(
+        {
+            "type": "folder",
+            "name": name,
+            "items": sorted(
+                [convert_to_book_folder(d) for d in source], key=itemgetter("name")
+            ),
+        }
+    )
+
+book_folders_order = [
+    "Condições",
+    "Raças",
+    "Classes",
+    "Origens",
+    "Perícias",
+    "Poderes",
+    "Equipamento",
+    "Magias",
+    "Testes",
+    "Habilidades",
+    "Combate",
+    "Tabelas",
+]
+book = sorted(book, key=lambda item: book_folders_order.index(item["name"]))
+
+abilities_and_powers = OrderedDict()
+for source in (
+    sorted(races, key=itemgetter("name"))
+    + sorted(classes, key=itemgetter("name"))
+    + sorted(powers, key=itemgetter("name"))
+):
+    for item in source.get("abilities", source.get("powers")):
+        extra = item.get("source")
+        abilities_and_powers[ 
+            f"{source['name']}{f' - {extra}' if extra else ''} - {item['name']}"
+        ] = item
+
+spells_circles = OrderedDict()
+for spell_circle in spells:
+    spells_circles[spell_circle["name"][0]] = OrderedDict()
+    for spell in sorted(spell_circle["spells"], key=itemgetter("name")):
+        spells_circles[spell_circle["name"][0]][spell["name"]] = spell
+
+with open(output_path, "w", encoding="utf-8") as jfile:
+    json.dump(
+        {
+            "book": book,
+            "abilities_and_powers": abilities_and_powers,
+            "spells": spells_circles,
+            "equipments": equipments,
+            "races": sorted(races, key=itemgetter("name")),
+        },
+        jfile,
+        ensure_ascii=False,
+    )
